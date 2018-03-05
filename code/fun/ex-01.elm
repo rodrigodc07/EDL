dd52f3627ff6331cc3f54d7543b047d5395d2211
rodrigodc07 <31592221+rodrigodc07@users.noreply.github.com>
2018-03-05 08:37:11 -0300

    Update ex-01.elm

--- code/fun/ex-01.elm
+++ code/fun/ex-01.elm
@@ -17,6 +17,25 @@ media (_,n1,n2) = (n1+n2)/2     -- o nome é ignorado
 nome: Aluno -> String
 nome (nm,_,_) = nm              -- as notas são ignoradas
 
+aprovado: Aluno -> Bool
+aprovado (_,n1,n2) = (((n1+n2)/2) > 7)
+
+gabaritou: Aluno -> Bool
+gabaritou (_,n1,_) = (n1==10)
+
+nome_med: Aluno -> (String,Float)
+nome_med (nm,n1,n2) = (nm,(n1+n2)/2)
+
+count: Aluno -> number -> number
+count x y = y + 1
+
+nota: Aluno -> (Float,Float)
+nota (_,n1,n2) = (n1,n2)
+
+f3: Aluno -> List Float -> List Float
+f3 (_,n1,n2) x = n2::(n1::x)
+
+
 -- Por fim, considere as assinaturas para as funções map, filter, e fold a seguir:
 
 --List.map: (a->b) -> (List a) -> (List b)
@@ -36,33 +55,33 @@ nome (nm,_,_) = nm              -- as notas são ignoradas
 -- Usando as definições acima, forneça a implementação para os três trechos marcados com <...>:
 
 turma: Turma
-turma = [ ("Joao",7,4), ("Maria",10,8), ... ]       -- 50 alunos
+turma = [ ("Joao",7,4), ("Maria",10,8), ("Paulinho",5,4), ("Josh",10,10) ]       -- 50 alunos
 
 -- a) LISTA COM AS MÉDIAS DOS ALUNOS DE "turma" ([5.5, 9, ...])
 medias: List Float
-medias = ...
+medias = List.map media turma
 
 -- b) LISTA COM OS NOMES DOS ALUNOS DE "turma" APROVADOS (["Maria", ...])
 aprovados: List String
-aprovados = ...
+aprovados = List.map nome (List.filter aprovado turma)
 
 -- c) MÉDIA FINAL DOS ALUNOS DE "turma" (média de todas as médias)
 total: Float
-total = ...
+total = List.foldl (+) 0 medias /  List.foldl count 0 turma
 
 -- d) LISTA DE ALUNOS QUE GABARITARAM A P1 ([("Maria",10,8), ...])
 turma_dez_p1: Turma
-turma_dez_p1 = ...
+turma_dez_p1 = List.filter gabaritou turma
 
 -- e) LISTA COM OS NOMES E MEDIAS DOS ALUNOS APROVADOS ([("Maria",9), ...])
 aprovados2: List (String,Float)
-aprovados2 = ...
+aprovados2 = List.map nome_med (List.filter aprovado turma)
 
 -- f) LISTA COM TODAS AS NOTAS DE TODAS AS PROVAS ([7,4,10,8,...])
 notas: List Float
-notas = ...
+notas = (List.foldl f3 [] turma)
 
 -- É permitido usar funções auxiliares, mas não é necessário.
 -- (As soluções são pequenas.)
 
-main = text (toString turma)
+main = text (toString notas)
